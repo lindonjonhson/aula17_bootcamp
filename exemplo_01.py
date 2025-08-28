b1e711fa791f3ed09b5a2cc590604700d626b5cb
Lindon Morais <lindon.jonhsonf@gmail.com>
2025-08-28 15:26:57 -0300

    Added class file

---
+++ exemplo_01.py
@@ -0,0 +1,37 @@
+
+
+# try:
+from sqlalchemy import create_engine
+
+# Conectar ao SQLite em memória
+engine = create_engine('sqlite:///meubanco.db', echo=True)
+
+print("Conexão com SQLite estabelecida.")
+# except Exception as e:
+#     print(e)
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'usuarios'
+    
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    idade = Column(Integer)
+
+# Criar as tabelas no banco de dados
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+novo_usuario = Usuario(nome='João', idade=28)
+session.add(novo_usuario)
+session.commit()
+
+print("Usuário inserido com sucesso.")
